64707d1d0b9981e3bc4a7c276e933f21b59d334d
바악경일 <129853254+pki071120@users.noreply.github.com>
2024-03-30 22:54:03 +0900

    [level 0] Title: 배열 만들기 6, Time: 3.74 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+    var stk = [];
+    let i = 0
+    while( i < arr.length)
+      if(stk.length === 0) {
+        stk.push(arr[i])
+        i++
+      }else{
+        if(stk[stk.length-1] === arr[i]) {
+          stk.pop()
+          i++
+      }else{
+          stk.push(arr[i])
+          i++
+        }
+      }
+    return stk.length === 0 ? [-1]: stk;
+}
